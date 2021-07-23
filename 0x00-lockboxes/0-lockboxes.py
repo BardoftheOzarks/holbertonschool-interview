0db094a86bf97c572f276565d611d109c6ac854f
BardoftheOzarks <1886@holbertonschool.com>
2021-07-23 03:47:04 +0000

    pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,11 +6,12 @@ def canUnlockAll(boxes):
     '''Let's crack some code'''
     keyring = [0]
     count = 0
-    while (len(boxes) > count):
-        for box in boxes:
+    while len(boxes) > count:
+        for box in range(len(boxes)):
             if box in keyring:
-                for key in box:
-                    keyring.append(key)
+                for key in boxes[box]:
+                    if key not in keyring:
+                        keyring.append(key)
         count += 1
         if len(boxes) == len(keyring):
             return True
