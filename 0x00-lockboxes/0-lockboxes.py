22ca0409cb561549377e521db74f8f46b50f0ab9
BardoftheOzarks <1886@holbertonschool.com>
2021-07-23 04:03:09 +0000

    added empty list conditional

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,14 +4,15 @@
 
 def canUnlockAll(boxes):
     '''Let's crack some code'''
+    if boxes is None or len(boxes) < 1:
+        return False
     keyring = [0]
     count = 0
     while len(boxes) > count:
-        for box in range(len(boxes)):
-            if box in keyring:
-                for key in boxes[box]:
-                    if key not in keyring:
-                        keyring.append(key)
+        for key in keyring:
+            for keys in boxes[key]:
+                if keys not in keyring and keys < len(boxes):
+                    keyring.append(keys)
         count += 1
         if len(boxes) == len(keyring):
             return True
