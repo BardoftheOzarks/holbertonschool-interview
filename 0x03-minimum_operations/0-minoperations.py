637127d2bd50abad19e77c4f4a5cd3ee41873437
BardoftheOzarks <1886@holbertonschool.com>
2021-08-12 23:06:30 +0000

    optimized space complexity

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -10,26 +10,20 @@ def minOperations(n):
         return 0
     if isprime(n):
         return n
-    factors = []
+
     opcount = 0
     chars = n
-    check = 1
     i = 2
 
     while i < (int(chars / 2) + 1):
-        # check for potential prime factors
+        # find prime factors
         if chars % i == 0 and isprime(i):
-            factors.append(i)
-            chars //= i
-            if isprime(chars):
-                factors.append(chars)
+            opcount += i  # opcount is equal to sum of prime factors
+            chars //= i  # decrease loop limit factor for time complexity
+            if isprime(chars):  # catch for final factor
+                opcount += chars
         else:
             i += 1
-    for factor in factors:
-        opcount += factor  # add all prime factors together for min ops
-        check *= factor
-    if check != n:  # check for missing duplicate factor
-        print("Duplicate factors missing")
     return opcount
 
 
