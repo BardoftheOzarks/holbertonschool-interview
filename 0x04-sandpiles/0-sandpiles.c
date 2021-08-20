3ae7bfc277fff143fb896825ca8a78888dd30653
BardoftheOzarks <1886@holbertonschool.com>
2021-08-19 21:18:46 -0500

    Removed tabs and spaces to satisfy linter

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -7,19 +7,19 @@
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-        int i, j, limit_passed;
+int i, j, limit_passed;
 
-        limit_passed = 0;
-        for (i = 0; i < 3; i++)
-                for (j = 0; j < 3; j++)
-                {
-                        grid1[i][j] = grid1[i][j] + grid2[i][j];
-                        grid2[i][j] = 0;
-                        if (grid1[i][j] > 3)
-                                limit_passed++;
-                }
-        if (limit_passed > 0)
-                sandpile(grid1, grid2);
+limit_passed = 0;
+for (i = 0; i < 3; i++)
+for (j = 0; j < 3; j++)
+{
+grid1[i][j] = grid1[i][j] + grid2[i][j];
+grid2[i][j] = 0;
+if (grid1[i][j] > 3)
+limit_passed++;
+}
+if (limit_passed > 0)
+sandpile(grid1, grid2);
 }
 
 /**
@@ -29,28 +29,28 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
  */
 void sandpile(int grid1[3][3], int grid2[3][3])
 {
-        int i, j;
+int i, j;
 
-        print_pile(grid1);
-        for (i = 0; i < 3; i++)
-        {
-                for (j = 0; j < 3; j++)
-                {
-                        if (grid1[i][j] > 3)
-                        {
-                                grid1[i][j] -= 4;
-                                if (i > 0)
-                                        grid2[i - 1][j] += 1;
-                                if (i < 2)
-                                        grid2[i + 1][j] += 1;
-                                if (j > 0)
-                                        grid2[i][j - 1] += 1;
-                                if (j < 2)
-                                        grid2[i][j + 1] += 1;
-                        }
-                }
-        }
-        sandpiles_sum(grid1, grid2);
+print_pile(grid1);
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (grid1[i][j] > 3)
+{
+grid1[i][j] -= 4;
+if (i > 0)
+grid2[i - 1][j] += 1;
+if (i < 2)
+grid2[i + 1][j] += 1;
+if (j > 0)
+grid2[i][j - 1] += 1;
+if (j < 2)
+grid2[i][j + 1] += 1;
+}
+}
+}
+sandpiles_sum(grid1, grid2);
 }
 
 /**
@@ -60,17 +60,17 @@ void sandpile(int grid1[3][3], int grid2[3][3])
  */
 void print_pile(int grid[3][3])
 {
-        int i, j;
+int i, j;
 
-        printf("=\n");
-        for (i = 0; i < 3; i++)
-        {
-                for (j = 0; j < 3; j++)
-                {
-                        if (j)
-                                printf(" ");
-                        printf("%d", grid[i][j]);
-                }
-                printf("\n");
-        }
+printf("=\n");
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+printf(" ");
+printf("%d", grid[i][j]);
+}
+printf("\n");
+}
 }
