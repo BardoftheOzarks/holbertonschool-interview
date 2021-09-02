d92b97fed267d90846cc5216bf4834be3d317b0c
BardoftheOzarks <1886@holbertonschool.com>
2021-09-02 01:02:21 +0000

    initial push

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Parses a log of status codes with Pirates of the Caribbean references"""
+import sys
+
+class parsley:
+    '''Methods and values for log parsing and flavor'''
+
+    def __init__(self):
+        '''Let's git init'''
+        self.totals = {'200': 0, '301': 0, '400': 0, '401': 0,
+                       '403': 0, '404': 0, '405': 0, '500': 0}
+        self.size = 0
+
+    def parsnip(self):
+        '''snips the log into firewood pieces'''
+        count = 0
+        for line in sys. stdin:
+            if line != '':
+                line = line.split()
+                self.size += int(line[-1])
+                self.totals[line[-2]] += 1
+                count += 1
+                if count % 10 == 0:
+                    self.parler()
+
+    def parler(self):
+        '''relays the totals back to the capt...er...user'''
+        print("File size: {}".format(self.size))
+        for response in sorted(self.totals.keys()):
+            if self.totals[response] > 0:
+                print("{}: {}".format(response, self.totals[response]))
+
+if __name__ == '__main__':
+    log = parsley()
+    try:
+        log.parsnip()
+    except KeyboardInterrupt:
+        log.parler()
