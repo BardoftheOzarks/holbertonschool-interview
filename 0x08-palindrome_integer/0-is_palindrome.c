580fc9173c0de4594b25c61e5681830106d3b8ea
BardoftheOzarks <1886@holbertonschool.com>
2021-09-15 17:20:21 +0000

    alternate solution, uses fewer resources

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -7,67 +7,17 @@
  */
 int is_palindrome(unsigned long n)
 {
-	unsigned long x = n;
-	listint_t *head = NULL, *tail = NULL, *current = NULL;
+	unsigned long copy = n, reverse = 0;
 
 	if (n < 10)
 		return (1);
-	while (x > 0)
+	while (copy > 0)
 	{
-		tail = add_node(&tail, x % 10);
-		if (head == NULL)
-			head = tail;
-		x = x / 10;
+		reverse *= 10;
+		reverse += copy % 10;
+		copy /= 10;
 	}
-	current = head;
-	while (current->n == tail->n)
-	{
-		if (current->next == tail || current == tail)
-		{
-			free_list(head);
-			return (1);
-		}
-		current = current->next;
-		tail = tail->prev;
-	}
-	free_list(head);
+	if (n == reverse)
+		return (1);
 	return (0);
-}
-
-/**
- * add_node - creates a node
- * @prev: previous node for linking
- * @n: int
- * Return: new node
- */
-listint_t *add_node(listint_t **prev, int n)
-{
-	listint_t *new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	new->prev = *prev;
-	if (*prev == NULL)
-		*prev = new;
-	else
-		(*prev)->next = new;
-	return (new);
-}
-
-/**
- * free_list - frees a list
- * @head: head node of list
- */
-void free_list(listint_t *head)
-{
-	listint_t *current;
-
-	while (head)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
 }
--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -4,22 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * struct listint_s - doubly linked list
- * @n: int
- * @next: pointer to next node
- * @prev: pointer to previous node
- * Description: doubly linked list node struct
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-	struct listint_s *prev;
-} listint_t;
-
 int is_palindrome(unsigned long n);
-listint_t *add_node(listint_t **prev, int n);
-void free_list(listint_t *head);
 
 #endif
